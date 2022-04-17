9cc4559f6bf2a808b09ad34ca720b7ae36606d3f
nurik <nurikbektursunovb@gmail.com>
2022-04-17 14:52:23 +0600

    added readme & requirements

--- questionnaire/settings.py
+++ questionnaire/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'questionnaire.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test2',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'NAME': DBNAME,
+        'USER': DBUSER,
+        'PASSWORD': DBPASSWORD,
+        'HOST': DBHOST,
+        'PORT': DBPORT,
     }
 }
 
